305ea61682af488965fd379e7336983d097643b9
Chris <54808483+cechua@users.noreply.github.com>
2023-03-18 17:24:53 +0800

    added js function for mousehover

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-function fillGrid() {
-    var gridCount = 16
+
+function mouseEnterEvent(e) {
+    e.classList.add('boxhovered')
+}
+
+function fillGrid(gridCount) {
+    var gridCount = Number(gridCount) <= 64 ? Number(gridCount) : 64
+    console.log(gridCount)
     var gridHTML = ''
     for(let i = 0; i < gridCount; i++) 
     {
         gridHTML+= '<div class="gridrow">'
         for(let j = 0; j < gridCount; j++)
         {
-            gridHTML+= '<div class="gridbox" id="gridBox' +j+'"></div>'
+            gridHTML+= '<div class="gridbox" id="gridBox' +j+'" onmouseenter="mouseEnterEvent(this)"></div>'
         }
         gridHTML+= '</div>'
     }
     document.getElementById('container').innerHTML = gridHTML
 }
 
-fillGrid();
+function promptUser(){
+    let gridCount = prompt('Enter a number for desired box count(max of 64).')
+    fillGrid(gridCount);
+}
+
